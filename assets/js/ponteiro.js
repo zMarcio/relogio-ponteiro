e4f3460bcef0d8e8380fc4bc6eeab2da5b04b402
zMarcio <marciopaulolima21@edu.unifor.br>
2023-05-05 18:10:26 -0300

    Relógio ponteiro

---
+++ assets/js/ponteiro.js
@@ -0,0 +1,29 @@
+const hoursElement = document.getElementById("hour_hand");
+const minutesElement = document.getElementById("minute_hand");
+const secondsElement = document.getElementById("second_hand");
+const textElement = document.getElementById("text");
+
+let showDate = true;
+
+function upDateTime(){
+    const data = new Date();
+    
+    const day = data.getDay();
+    const ampm = data.getHours() >= 12 ? "PM" : "AM";
+    const hour = data.getHours() + data.getMinutes()/60;
+    const minute = data.getMinutes() + data.getSeconds()/60;
+    const second = data.getSeconds() + data.getMilliseconds()/1000;
+
+    textElement.textContent = showDate ? day : ampm;
+    hoursElement.setAttribute("transform", `rotate(${(360/12)*hour})`);
+    minutesElement.setAttribute("transform", `rotate(${(360/60)*minute})`);
+    secondsElement.setAttribute("transform", `rotate(${(360/60)*second})`);
+
+    requestAnimationFrame(upDateTime);
+}
+
+requestAnimationFrame(upDateTime);
+
+textElement.addEventListener("click", () =>{
+    showDate = !showDate;
+})
